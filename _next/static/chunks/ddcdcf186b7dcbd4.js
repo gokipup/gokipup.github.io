25d58106ade7de2ec6cdaf3f15f084b0a1a64aac
rwaltenberg <rwaltenberg@users.noreply.github.com>
2026-04-24 03:23:56 +0000

    deploy: gokipup/webapp@8922a6ff669482ed173cfbd800698ee130ba581e

---
+++ _next/static/chunks/ddcdcf186b7dcbd4.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,34779,t=>{"use strict";var e=t.i(48752);function o({path:t}){return(0,e.useEffect)(()=>{window.location.replace(`https://app.gokipup.com${t}`)},[t]),null}t.s(["AppRedirect",()=>o])}]);
